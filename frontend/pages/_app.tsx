399407a87295992038c48a4abac9e4f44c4cc965
Abhishek <abhishekkumar214567@gmail.com>
2024-08-01 20:12:56 +0530

    install thirdweb

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,26 @@
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { hederaTes } from "@thirdweb-dev/wallets";
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+
+  return (
+    <ThirdwebProvider 
+        activeChain={hederatestnet} 
+        supportedChains={[hederatestnet]}
+        clientId={process.env.NEXT_PUBLIC_THIRDWEB_KEY}
+        >
+      <ChakraProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </ChakraProvider>
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
