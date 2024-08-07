f320bffb227f3c4e52024a6a897f43ac7a70b07e
Abhishek <abhishekkumar214567@gmail.com>
2024-08-08 04:02:56 +0530

    build the components for transferring

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,15 +3,15 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { hederaTes } from "@thirdweb-dev/wallets";
+import { HederaTestnet } from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
 
   return (
     <ThirdwebProvider 
-        activeChain={hederatestnet} 
-        supportedChains={[hederatestnet]}
+        activeChain={HederaTestnet} 
+        supportedChains={[HederaTestnet]}
         clientId={process.env.NEXT_PUBLIC_THIRDWEB_KEY}
         >
       <ChakraProvider>
